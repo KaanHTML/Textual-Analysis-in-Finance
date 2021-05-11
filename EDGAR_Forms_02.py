6c34d631db332a60bbf30b0f93b786da0c2d7250
KaanHTML <59143443+KaanHTML@users.noreply.github.com>
2021-05-11 16:48:00 +0200

    Update EDGAR_Forms_02.py

--- EDGAR_Forms_02.py
+++ EDGAR_Forms_02.py
@@ -10,9 +10,7 @@ import requests
 
 def download_masterindex(year, qtr, flag=False):
     # Download Master.idx from EDGAR
-    # Loop accounts for temporary server/ISP issues
-    # ND-SRAF / McDonald : 201606
-
+    
     from urllib.request import Request, urlopen
 
     PARM_ROOT_PATH = 'https://www.sec.gov/Archives/edgar/full-index/'
@@ -31,13 +29,14 @@ def download_masterindex(year, qtr, flag=False):
 #Creating base_URL 
 base_url = 'https://www.sec.gov/Archives/'
 
+#looping over the years
 for year in range(2005, 2021):
          
         #creating new folder              
         #directory
         directory = str(year)
         
-        # Parent Directory path
+        # Directory path where you want to store the 10-K txt files
         parent_dir = "D:/Python_10K_Analysis/Test01"
         
         # Path
@@ -59,9 +58,11 @@ for year in range(2005, 2021):
         for item in masterindex[1:]:
         
              components = item.split('|')
-
+             
+             # search for the item 10-K since I only want annual reports
              if "10-K" in components:
-        
+                    
+                 # in the following I am creating the URL
                  components_norm = components[-1].replace("-", "")
         
                  url_components = components[-1].split("/")
@@ -75,19 +76,23 @@ for year in range(2005, 2021):
                  fourth_term_for_url = components_norm.split("/")[-1].split(".")[0]
         
                  fifth_term_for_url = url_components[3]
-        
+                 
+                 #this is the final url, which will be used to download from EDGAR server
                  full_url = base_url + first_term_url + '/' + second_term_url + '/' + third_term_url + '/' + fourth_term_for_url + '/' + fifth_term_for_url 
         
                  if url_components[2] not in dict_list:
                      
                      dict_list[url_components[2]] = full_url
-               
+                        
+                 #downloading the 10K from the newly created URL
                  myfile = requests.get(full_url)
 
                  created_file = open(str(path) + '/' + str(third_term_url) + '.txt', 'wb').write(myfile.content)
                  
+                 #due to parsing error, I am also calculating the file size of each txt 
                  size = os.path.getsize(str(path) + '/' + str(third_term_url) + '.txt')
                  
+                #if file size to small (<10000), it will be deleted
                  if size < 10000:
                      os.remove(str(path) + '/' + str(third_term_url) + '.txt')
         
